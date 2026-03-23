b51ffd0705e02ddaae267af8ab43f9cb49a009d3
laisdasilvadias123 <laisdasilvadias1232gmail.com>
2026-03-23 09:38:05 -0300

    Melhora formatacao da saida de dados

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+ 
 int main() {
-    printf("=== Integrantes do Grupo ===\n");
-    printf("Lais\n");
-    printf("RM: 569943\n");
-
-
+    printf("========================================\n");
+    printf("   CHECKPOINT 1 - GIT, GITHUB E C      \n");
+    printf("========================================\n");
+    printf("Integrantes do Grupo: Lais e João\n\n");
+ 
+    // Exibição dos nomes conforme exigido no PDF
+    printf("1. Lais da Silva Dias - RM: 569943\n");
+    printf("2. João Augusto Poloniato Telles - RM: 571443\n");
+ 
+    printf("\n========================================\n");
+    
     return 0;
 }
